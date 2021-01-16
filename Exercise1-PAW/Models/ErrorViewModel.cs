dcf7e4d657c8593644957f6b3fc13c09975d6580
Zhega Fao Anggasta <zhega.fao.2016@ft.umy.ac.id>
2021-01-16 22:50:13 +0700

    Add project files.

---
+++ Exercise1-PAW/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exercise1_PAW.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
